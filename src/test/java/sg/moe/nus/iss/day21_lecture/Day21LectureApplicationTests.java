d3c12fb88265ddf98271968f08baf9f10c7ae848
shiyun-ho <serene.hoshiyun@gmail.com>
2023-03-09 13:41:14 +0800

    first commit

---
+++ src/test/java/sg/moe/nus/iss/day21_lecture/Day21LectureApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.moe.nus.iss.day21_lecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day21LectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
